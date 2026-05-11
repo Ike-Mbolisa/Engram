89a8a798ec9dc477e8fa3863ec21751c279f7f47
Ike <ike.cadworks@gmail.com>
2026-05-11 14:44:03 +0200

    Logic fixed / Logik rettelse
    Logic has been updated to be more coherent.
    
    /
    
    Logikken er bleven oprettet så det hele hænger sammen.

--- script.py
+++ script.py
@@ -9,9 +9,10 @@ ____________________________________
 Indlæser info om bilen fra en .json fil, hvor vi har de rigtige gearforhold, hovedgear og dækstørrelse
 '''
 def load_car(path: str) -> dict:
-    with open(path) as f: 
-        print(json.make + json.model + json.year)
-        return json.load(f)
+    with open(path) as f:
+        car = json.load(f)
+        print(f"{car['year']} {car['make']} {car['model']}")
+        return car
 
 '''
 Function for loading the carWe group all the sensor data into function, 
@@ -28,28 +29,27 @@ Her får vi rpm og fart data, som der bliver brugt i andre funktioner
 '''
 def read_obd() -> tuple[float, float]:
     connection = obd.OBD()
+    if not connection.is_connected():
+        raise ConnectionError("No OBD connection available")
     Revolutions = connection.query(obd.commands.RPM).value.magnitude
-    Velocity = connection.query(obd.commands.SPEED).value.to("km/h").magnitude 
-    if connection == False:
-        export_csv()    
-    
+    Velocity = connection.query(obd.commands.SPEED).value.to("km/h").magnitude
     return Revolutions, Velocity
 
-def live_gear_estimate() -> int:
+def live_gear_estimate(car: dict) -> int:
     Revolutions, Velocity = read_obd()
-    return estimate_gear(Revolutions, Velocity)
+    return estimate_gear(Revolutions, Velocity, car)
 
 '''
 Since OBD does not send gear data, we will estimate with the gear ratio, speed and tyre size
 ____________________________________
 Vi gætter på hvilket gear man er i via. gearforhold, fart og dæk størrelse 
 '''
-def estimate_gear(Revolutions, Velocity): 
+def estimate_gear(Revolutions, Velocity, car: dict):
    if Velocity == 0:
        return 0
    Velocity_ms = Velocity / 3.6
-   calculated_ratio = (Revolutions / 60 * json.tyre_circumference) / (Velocity_ms * json.final_drive)
-   closest_gear = min(json.gear_ratio, key=lambda g: abs(json.gear_ratio[g] - calculated_ratio))
+   calculated_ratio = (Revolutions / 60 * car['tyre_circumference']) / (Velocity_ms * car['final_drive'])
+   closest_gear = min(car['gear_ratios'], key=lambda g: abs(car['gear_ratios'][g] - calculated_ratio))
    return closest_gear
 '''
 The data is exported as a CSV file called output.csv
@@ -58,7 +58,7 @@ ____________________________________
 Dataen bliver eksporteret som en CSV file der hedder output.csv
 we definer kolonnerne, der dermed skal bruges til at oprette csv strukturen
 '''
-def export_csv(input_file: str, output_file: str = 'output.csv'):
+def export_csv(input_file: str, car: dict, output_file: str = 'output.csv'):
     mdf = MDF(input_file)
     
     df = mdf.to_dataframe()
@@ -68,7 +68,7 @@ def export_csv(input_file: str, output_file: str = 'output.csv'):
     
     if Revolutions_col and Velocity_col:
         df['gear'] = df.apply(
-            lambda row: estimate_gear(row[Revolutions_col], row[Velocity_col]), axis=1
+            lambda row: estimate_gear(row[Revolutions_col], row[Velocity_col], car), axis=1
         )
     
     df.to_csv(output_file)
